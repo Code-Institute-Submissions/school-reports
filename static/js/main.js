46c317fa40a38f8d462057c7b69ed7d2579e9707
Richard Stainton <richardwstainton@hotmail.com>
2018-09-02 13:51:20 +0000

    date pickers and collapsibles

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+   $('.datepicker').datepicker();
+   $('textarea#Details').characterCounter();
    $('select').formSelect();
    $('.modal').modal();
    $('.collapsible').collapsible();
    $('.button-collapse').sideNav();
-    
+   
 });
 
